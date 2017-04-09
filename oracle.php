566e04a84a5434b30fc8a9c62f8857e74ade4dcd
Adtiv <datocco13@gmail.com>
2017-04-08 21:11:42 -0400

    html page shows php queries

--- oracle.php
+++ oracle.php
@@ -1,5 +1,6 @@
 #!/usr/local/bin/php
 <?php
+
 // Create connection to Oracle
 $connection = oci_connect($username = 'ztaylor', $password = 'mypassword', $connection_string = '//oracle.cise.ufl.edu/orcl');
 if (!$connection) {
@@ -7,16 +8,32 @@ if (!$connection) {
    echo $m['message'], "\n";
    exit;
 }
-else {
-   print "Sucessfully connected to Oracle!\n";
+else {  
+    //ini_set('display_errors', 'On');
+    //error_reporting(E_ALL);
 
-    $statement = oci_parse($connection, 'SELECT * FROM stats');
+    //$Player ="Player";
+    $Team = "%" . $_POST['Team'] . "%";
+    $query = "SELECT DISTINCT * FROM Player 
+    WHERE TeamName like :Team 
+    ORDER BY Year, TeamName, PlayerName";
+    $statement = oci_parse($connection, $query);
+    oci_bind_by_name($statement, ":Team", $Team);
     oci_execute($statement);
-
-    while (($row = oci_fetch_object($statement))) {
-        var_dump($row);
-    }
+    echo "$Team";
+    echo"<table border='2'>";
+    echo "<tr><th>Player</th>
+        <th>Team</th>
+        <th>Offense/Defense</th>
+        <th>Category</th>
+        <th>Year</th></tr>";
+  while ($row=oci_fetch_row($statement))
+  { 
+        echo"<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td></tr>";
+  }
+     echo"</table></br>";
+     oci_free_statement($statement);
 }
-oci_free_statement($test);
+oci_commit($connection);
 oci_close($connection);
 ?>
